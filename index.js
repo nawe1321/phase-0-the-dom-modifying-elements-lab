0cd84c07cf41e0d3ffdfc0b611563a33af2cc93c
Nathan <nathan.wendlowsky@flatironschool.com>
2023-07-18 14:43:08 -0700

    prework lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+
+const myName = 'Nathan'
+newHeader.textContent = `${myName} is the champion`;
